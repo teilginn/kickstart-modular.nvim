abc626246a6470a2e27b7af2689eaa877aeed9d4
François TERROT <fterrot@april.org>
2026-02-28 08:45:28 +0100

    vscode protected nerd font

--- init.lua
+++ init.lua
@@ -93,17 +93,21 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
--- [[ Setting options ]]
-require 'options'
+if vim.g.vscode then
+  vim.g.have_nerd_font = true
+else
+  -- [[ Setting options ]]
+  require 'options'
 
--- [[ Basic Keymaps ]]
-require 'keymaps'
+  -- [[ Basic Keymaps ]]
+  require 'keymaps'
 
--- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+  -- [[ Install `lazy.nvim` plugin manager ]]
+  require 'lazy-bootstrap'
 
--- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+  -- [[ Configure and install plugins ]]
+  require 'lazy-plugins'
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
